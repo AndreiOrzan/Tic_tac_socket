4bb65b47ddc58522bcca205e3c50aee8a51bd02a
AndreiOrzan <48299309+AndreiOrzan@users.noreply.github.com>
2019-03-17 20:57:37 +0000

    Update clienttic.py
    Game restart after win/draw.
    
    Player that loses goes first on the second round . 
    Still need to implement a swap for the X - O after a new game .

--- clienttic.py
+++ clienttic.py
@@ -1,12 +1,60 @@
 import socket
 
-#def again(dic,soc):
-#    restart =  raw_input("PLay again? Y/N")
-#    if restart == "Y":
-#        dic={"id":1,"mat":[[0,0,0],[0,0,0],[0,0,0]],"Win":0,"restart":"Y" }
-#        print dic
-#        soc.send(str(dic))
 
+mat =[]
+
+
+def matrix(mat):
+    if mat[0][0]==mat[1][1]==mat[2][2]!=0:
+        return mat[0][0]
+    if mat[0][2]==mat[1][1]==mat[2][0]!=0:
+        return mat[1][1]
+    for i in range (3):
+        if mat[i][0]==mat[i][1]==mat[i][2]!=0:
+            return mat[i][0]
+        if mat[0][i]==mat[1][i]==mat[2][i]!=0:
+            return mat[0][i]
+            
+
+def win_check(dic):
+    if dic["Win"][1] == 8: #remiza check
+        dic["Win"][0] = 3
+    
+    if matrix(dic["mat"])==1 or matrix(dic["mat"])==2:
+        dic["Win"][0] = matrix(dic["mat"])
+
+        
+def print_win(dic,soc,player,test):
+
+    win_check(dic)
+    
+    if dic["Win"][0] != 0:
+        if dic["Win"][0] == player :
+            #soc.send(str(dic))
+            print "You have won"
+            test = 1
+            dic["restart"] = True
+            print dic
+            
+            
+            #dic = {"id":1,"mat":[[0,0,0],[0,0,0],[0,0,0]],"Win":[0,0],"restart":True}
+            
+            
+
+
+        elif dic["Win"][0] == 3:
+            print "Draw"
+        else:
+            print "You have lost"
+            print dic
+            if dic["restart"] == True:# restarting and 
+                dic["Win"]=[0,0]
+                dic["mat"]=[[0,0,0],[0,0,0],[0,0,0]]
+            print dic
+            
+            
+                
+            
 def draw(mat):
     xy=[" ","X","O"]
     print ".___"*3+"."
@@ -18,53 +66,69 @@ def draw(mat):
 def client():
     HOST = 'localhost'    # The remote host
     PORT = 50007              # The same port as used by the server
-    test = 1
+    
     soc =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     soc.connect((HOST, PORT))
     
+    test = 1
+    
     while True:
-        
+        i,j = 0,0
         dic = eval(soc.recv(1024))
+        
         
         if test == 1 :
             player = dic["id"]
             print "You are player ",dic["id"]
+            if player == 1:
+                other = 2
+            else:
+                other = 1 
             test = 0
+            
+
         else:
-        
+            print_win(dic,soc,player,test)
             mat = dic["mat"]
-            if dic["Win"] != 0:
-                if dic["Win"] == player :
-                    print "You have won"
+            
+            draw(dic["mat"])
 
-
-                elif dic["Win"] == 3:
-                    print "Draw"
-                else:
-                    print "You have lost"
-                   
-                soc.send(str(dic))
                 
-                    
-            draw(mat)
-            if dic["Win"] != 0:
-                break
             while True:
-                i = int(raw_input("row :"))
+                
+
+                
                 while i < 1 or i > 3 :
-                    i = int(raw_input("row :"))
+                    try: 
+                        i = int(raw_input("row :"))
+                    except: 
+                        print "please type in a valid input "
+                    
                 i = i - 1
-                j = int(raw_input("column :"))
+                
                 while j < 1 or j > 3:
-                    j = int(raw_input("column :"))
+                    try: 
+                        j = int(raw_input("column :"))
+                    except: 
+                        print "please type in a valid input "
                 j = j - 1
+                
                 if mat[i][j] == 0:
                     mat[i][j] = player
                     break
                 else:
                     print "Position is occupied"
-            
+                    
             dic["mat"] = mat
+            print_win(dic,soc,player,test)
+            
+            if dic["Win"][0] == 0:
+                print "\n It is Player "+str(other)+"'s turn, please wait..\n"
+
+            
+            
+            
             soc.send(str(dic))
+            
 client()
